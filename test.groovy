e0f79f45780232d7bd78201cb4425b96964de6a4
julian1 <julian.atkinson71@gmail.com>
2014-11-04 15:19:15 +1100

    add test cases

---
+++ test.groovy
@@ -0,0 +1,24 @@
+
+//println vocab.Organisation.list().name
+//println vocab.Organisation.get(1).name
+
+//println vocab.Person.list().name
+//println vocab.Person.get(1).name
+
+//println vocab.AffiliationType.list().name
+//println vocab.AffiliationType.get(1).name
+
+//println vocab.OrganisationSynonym.list().name
+// table is empty
+
+println vocab.ReferenceSource.list().citationString
+println vocab.ReferenceSource.get(1).citationString
+
+println vocab.ResponsibleParty.list().person.name
+println vocab.ResponsibleParty.get(1).person.name
+
+println vocab.ClassificationScheme.list().name
+println vocab.ClassificationScheme.get(1).name
+
+println vocab.VocabularyRegister.get( 1) 
+
