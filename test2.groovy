7a6b8207e547d5ef8927e41d71caf4d053dfd1b8
julian1 <julian.atkinson71@gmail.com>
2014-11-05 16:07:19 +1100

    update test2 to handle term saving

--- test2.groovy
+++ test2.groovy
@@ -15,3 +15,22 @@ rp.organisation = o
 rp.save( flush: true, failOnError:true )
 
 
+// change amendment type
+a = vocab.Amendment.get( 1)
+a.type = 'removing'
+a.save( flush: true, failOnError:true )
+
+// change amendment responsible party
+a.responsibleParty = vocab.ResponsibleParty.get( 3)
+a.isDirty()
+a.save( flush: true, failOnError:true )
+
+
+// change vocab term
+term = vocab.VocabularyTerm.find( "from VocabularyTerm where uid = 'http://vocab.nerc.ac.uk/collection/L22/current/TOOL0665' " )
+term.definition = 'whoot'
+term.isDirty()
+term.save( flush: true, failOnError:true )
+
+
+
